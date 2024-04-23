1c957fbf6d33251f50a5d64b2d574b0bad10d081
miljana20 <87268657+miljana20@users.noreply.github.com>
2024-04-23 08:17:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $('#hamburgercic').on('click', function(){
-        $('#mini-nav').toggle();
+        $('#mini-nav').fadeToggle();
     });
+
+    $('#authh').on('click', function(){
+        $('#auth-div').fadeToggle('slow');
+    });
+
     $('li.category').mouseenter(function(){
         var targetDivId = $(this).data('target');
         $('.cat-div').css('display', 'none');
@@ -17,4 +22,23 @@ $(document).ready(function(){
         $(this).css('--value', value + '%');
     });
 
+    $('.front').css('transform', 'rotateY(360deg)');
+    setTimeout(()=>{
+        $("#preloader").fadeOut(2000, function(){
+            $("#preloader").remove();
+        });
+    }, 1200)
+
+    $('#to-top').click(function(){
+        $('html ,body').animate({scrollTop : 0});
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 300){
+            $('#to-top').fadeIn();
+        }
+        else{
+            $('#to-top').fadeOut();
+        }
+    });
 });
